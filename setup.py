882c719b26178367ce43ca7fcc6d1bf41bec003e
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2019-08-05 16:31:39 +0200

    - update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
+import os
+
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='python-e3dc',
     version='0.0.1',
-    packages=[''],
+    packages=['python_e3dc'],
     url='https://www.github.com/python-e3dc',
-    license='',
+    license='MIT',
     author='J. Brunswicker',
-    author_email='',
-    description='', install_requires=['py3rijndael', 'libscrc']
+    author_email='johannes.brunswicker@gmail.com',
+    description='',
+    long_description=read('README.md'),
+    install_requires=['py3rijndael', 'libscrc'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Libraries"
+    ]
 )
